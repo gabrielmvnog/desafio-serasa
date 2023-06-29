20f42d7130af30c7332767a313feb372083edc86
Gabriel Nogueira <gabrielmvnogueira@gmail.com>
2023-06-29 13:45:52 -0300

    chore: initial setup for postgres, sqlalchemy and alembic in user-api

---
+++ user-api/app/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings, PostgresDsn
+
+
+class Settings(BaseSettings):
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
+
+
+settings = Settings()
